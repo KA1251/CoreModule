5d83106518d56f11e4bb957fa830cfe9d7e57b8d
1251 <100079410+KA1251@users.noreply.github.com>
2024-03-11 17:24:24 +0300

    fixes+ref

--- cockroach.go
+++ cockroach.go
@@ -9,18 +9,20 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func ConToCockRoach(drivername, username, password, dbname, host, port, appname string, done chan<- struct{}, data chan<- *sql.DB) {
+func ConToCockRoach(drivername, username, password, dbname, host, port, appname string, done chan<- struct{}, data chan<- *sql.DB, con *ConnectionHandler) {
 	for {
 		dsn := fmt.Sprintf("postgresql://%s@%s:%s/%s?sslmode=disable&application_name=%s", username, host, port, dbname, appname)
-		db, err := sql.Open(drivername, dsn)
-		if err == nil {
+		db, _ := sql.Open(drivername, dsn)
+		con.CockroachErr = db.Ping()
+		if con.CockroachErr == nil {
 			logrus.Info("COCKROACH sucsessfull conection")
 			data <- db
 			done <- struct{}{}
 			return
+		} else {
+			logrus.Error("Error during connection to COCKROACHDB", con.CockroachErr)
+			time.Sleep(3 * time.Second)
 		}
-		logrus.Error("Error during connection to COCKROACHDB", err)
-		time.Sleep(3 * time.Second)
 
 	}
 }
