f045a339dc120135957204cedcae6c34ea970af2
1251 <100079410+KA1251@users.noreply.github.com>
2024-03-11 17:27:26 +0300

    fixes+err field added

--- core.go
+++ core.go
@@ -2,14 +2,13 @@ package core
 
 import (
 	"database/sql"
-	"log"
 	"sync"
 
 	"github.com/Shopify/sarama"
-	"github.com/coder/redjet"
 	"github.com/go-redis/redis/v8"
 	"github.com/jmoiron/sqlx"
 	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
+	"github.com/sirupsen/logrus"
 	"github.com/streadway/amqp"
 )
 
@@ -21,27 +20,29 @@ type ConnectionHandler struct {
 
 	PrometheusIsInitialized bool
 	Prometheus              *v1.API
+	Prometheus_err          error
 
 	// SQLDB
 	SQLDBIsInitialized bool
 	SQLDB              *sqlx.DB
+	SQLDBErr           error
 
 	// Redis
 	RedisIsInitialized bool
 	Redis              *redis.Client
-
-	// Redis-Redjet
-	RedisRedjetIsInitialized bool
-	RedisRedjet              *redjet.Client
+	RedisErr           error
 
 	// RabbitMQ
 	RabbitMQIsInitialized bool
 	RabbitMQ              *amqp.Connection
+	RabbitMQErr           error
 
 	// Kafka
 	KafkaIsInitialized bool
 	KafkaProducer      sarama.SyncProducer
 	KafkaConsumer      sarama.Consumer
+	KafkaConsumerErr   error
+	KafkaProducerErr   error
 
 	// HealthCheck
 	HealthCheckIsInitialized bool
@@ -49,10 +50,12 @@ type ConnectionHandler struct {
 	//MySQL
 	MySQLIsInitialized bool
 	MySQLDB            *sql.DB
+	MySQLDBErr         error
 
 	//Cockroach
 	CockroachIsInitialized bool
 	Cockroach              *sql.DB
+	CockroachErr           error
 }
 
 var Handler ConnectionHandler
@@ -70,45 +73,50 @@ func (h *ConnectionHandler) CloseAllConnections() {
 	if h.SQLDB != nil {
 		h.SQLDBIsInitialized = false
 		if err := h.SQLDB.Close(); err != nil {
-			log.Printf("Error when closing connection with CockroachDB: %s\n", err)
+			logrus.Error("Error when closing connection with SQLDB:", err)
+		} else {
+			logrus.Info("SQLDB was closed")
 		}
 	}
 
 	if h.Redis != nil {
 		h.RedisIsInitialized = false
 		if err := h.Redis.Close(); err != nil {
-			log.Printf("Error when closing connection with Redis: %s\n", err)
-		}
-	}
-
-	if h.RedisRedjet != nil {
-		h.RedisRedjetIsInitialized = false
-		if err := h.RedisRedjet.Close(); err != nil {
-			log.Printf("Error when closing connection with Redis-Redjet: %s\n", err)
+			logrus.Error("Error when closing connection with Redis:", err)
+		} else {
+			logrus.Info("Redis was closed")
 		}
 	}
 
 	if h.RabbitMQ != nil {
 		h.RabbitMQIsInitialized = false
 		if err := h.RabbitMQ.Close(); err != nil {
-			log.Printf("Error when closing connection with RabbitMQ: %s\n", err)
+			logrus.Error("Error when closing connection with RabbitMQ:", err)
+		} else {
+			logrus.Info("CockroachDB was closed")
 		}
 	}
 	if h.Cockroach != nil {
 		h.CockroachIsInitialized = false
 		if err := h.Cockroach.Close(); err != nil {
-			log.Printf("Error when closing connection with Cockroach: %s", err)
+			logrus.Error("Error when closing connection with CockroachDB:", err)
+		} else {
+			logrus.Info("CockroachDB closed")
 		}
 	}
 	if h.KafkaProducer != nil {
 		if err := h.KafkaProducer.Close(); err != nil {
-			log.Printf("Error when closing Kafka Producer: %s\n", err)
+			logrus.Error("Error when closing connection with KafkaProducer:", err)
+		} else {
+			logrus.Info("KafkaProducer was closed")
 		}
 	}
 
 	if h.KafkaConsumer != nil {
 		if err := h.KafkaConsumer.Close(); err != nil {
-			log.Printf("Error when closing Kafka Consumer: %s\n", err)
+			logrus.Error("Error when closing connection with KafkaConsumer:", err)
+		} else {
+			logrus.Info("KafkaConsumer was closed")
 		}
 	}
 
