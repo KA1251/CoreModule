8af44056be54abf7662eb7b4a99161d157f66a2b
1251 <100079410+KA1251@users.noreply.github.com>
2024-03-13 17:50:46 +0300

    kafka consumergroup added

--- kafka.go
+++ kafka.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/Shopify/sarama"
@@ -8,41 +9,46 @@ import (
 )
 
 // NewKafka creates a new connection to Kafka
-func ConToKafka(host, port, username, password string, done chan<- struct{}, dataProd chan<- sarama.SyncProducer, dataCons chan<- sarama.Consumer, con *ConnectionHandler) {
+func ConToKafka(host, port, username, password string, done chan<- struct{}, dataProd chan<- sarama.SyncProducer, dataCons chan<- sarama.ConsumerGroup, con *ConnectionHandler) {
 
 	for {
 		// Setting up Kafka Producer
 		brokers := []string{host + ":" + port}
 
 		producerConfig := sarama.NewConfig()
-		producerConfig.Net.SASL.Enable = true
+		producerConfig.Producer.Return.Successes = true
+		/*producerConfig.Net.SASL.Enable = false
 		producerConfig.Net.SASL.User = username
-		producerConfig.Net.SASL.Password = password
+		producerConfig.Net.SASL.Password = password*/
 
 		consumerConfig := sarama.NewConfig()
-		consumerConfig.Net.SASL.Enable = true
-		consumerConfig.Net.SASL.User = username
-		consumerConfig.Net.SASL.Password = password
 
+		/*consumerConfig.Net.SASL.Enable = false
+		consumerConfig.Net.SASL.User = username
+		consumerConfig.Net.SASL.Password = password*/
+		consumerConfig.Consumer.Return.Errors = true
+		consumer, errConsumer := sarama.NewConsumerGroup(brokers, "group", consumerConfig)
 		producer, errProducer := sarama.NewSyncProducer(brokers, producerConfig)
-		consumer, errConsumer := sarama.NewConsumer(brokers, consumerConfig)
+		//consumer, errConsumer := sarama.NewConsumer(brokers, consumerConfig)
 
 		if errConsumer == nil || errProducer == nil {
 			logrus.Info("Kafka producer sucsessfull connection")
-			dataProd <- producer
 			logrus.Info("Kafka consumer sucsessful connection")
+			dataProd <- producer
 			dataCons <- consumer
 			done <- struct{}{}
+
 			return
 		}
 		if errProducer != nil || errConsumer != nil {
 			if errProducer != nil {
-				con.KafkaProducerErr = errProducer
-				logrus.Error("Error during connection to KAFKA(producer)", con.KafkaProducerErr)
+				fmt.Print("err:", errProducer)
+				//con.KafkaProducerErr = errProducer
+				logrus.Error("Error during connection to KAFKA(producer)", errProducer)
 			}
 			if errConsumer != nil {
-				con.KafkaConsumerErr = errProducer
-				logrus.Error("Error during connection to KAFKA(consumer)", con.KafkaConsumerErr)
+				//con.KafkaConsumerErr = errProducer
+				logrus.Error("Error during connection to KAFKA(consumer)", errConsumer)
 			}
 
 		}
